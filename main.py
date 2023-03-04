ff18494d9514a5586c81357c5f1efdd3510d3ca9
frothywater <kokoroic@outlook.com>
2023-03-04 09:36:32 +0000

    fix: Load checkpoint state directly

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import inspect
 from typing import Optional
+
+import torch
 from lightning.pytorch.cli import LightningCLI
+
 from melody_pretrain.dataset import MelodyPretrainDataModule
 
 
 class CustomLightningCLI(LightningCLI):
     """Lightning CLI does not support loading from checkpoint from now, so we need to override it."""
+
     def add_arguments_to_parser(self, parser) -> None:
         parser.add_argument("--load_from_checkpoint", type=Optional[str], default=None)
 
     def before_fit(self) -> None:
         load_from_checkpoint_path = self.config.fit.load_from_checkpoint
         if load_from_checkpoint_path is not None:
-            model_class = type(self.model)
-            model_config = self.config.fit.model
-            model_args = model_config.init_args if "init_args" in model_config else model_config
-            self.model = model_class.load_from_checkpoint(
-                load_from_checkpoint_path,
-                **model_args,
-                strict=False,
-            )
+            checkpoint = torch.load(load_from_checkpoint_path)
+            self.model.load_state_dict(checkpoint["state_dict"])
             print("Loaded model from checkpoint:", load_from_checkpoint_path)
 
 
