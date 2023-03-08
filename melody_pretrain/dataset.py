247d7806388c07dd1c1fd6b150b01bb992d365fb
frothywater <kokoroic@outlook.com>
2023-03-08 18:15:55 +0800

    Revert "feat: No need to set ngram type for each data item"
    This reverts commit 24b085e207005775fd1d0b2587c2e52293221d74.

--- melody_pretrain/dataset.py
+++ melody_pretrain/dataset.py
@@ -61,7 +61,7 @@ class DataBatch(NamedTuple):
     attention_mask: Optional[torch.Tensor] = None
 
     # Used for explicit ngram prediction
-    ngram_type: Optional[str] = None
+    ngram_types: Optional[List[str]] = None
     ngram_ids: Optional[torch.Tensor] = None
 
     # Used for permutated span prediction
@@ -959,7 +959,7 @@ class DataCollatorForPrefixMaskedLanguageModeling(DataCollator):
         # collect all the data, and genereate the inputs and labels
         inputs, labels = [], []
         source_lengths, input_lengths = [], []
-        ngram_ids_list = []
+        ngram_ids_list, ngram_types = [], []
         mask_positions_list, sep_positions_list, target_span_lengths_list = [], [], []
         for data, extra_data, offset in zip(data_list, extra_data_list, offsets):
             infilling_data = self.masking.mask_for_infilling(data, offset=offset, **extra_data)
@@ -987,7 +987,7 @@ class DataCollatorForPrefixMaskedLanguageModeling(DataCollator):
             target_span_lengths_list.append(target_span_lengths)
 
             ngram_ids_list.append(infilling_data.ngram_ids)
-            ngram_type = infilling_data.ngram_type
+            ngram_types.append(infilling_data.ngram_type)
 
         # pad
         input_ids = torch.from_numpy(np.stack(self.pad(inputs), axis=0)).long()
@@ -1022,7 +1022,7 @@ class DataCollatorForPrefixMaskedLanguageModeling(DataCollator):
                 dim=0,
             )
         else:
-            ngram_type = None
+            ngram_types = None
             ngram_ids = None
 
         # span indices for advanced infilling
@@ -1044,7 +1044,7 @@ class DataCollatorForPrefixMaskedLanguageModeling(DataCollator):
             label_ids,
             padding_mask,
             attention_mask,
-            ngram_type=ngram_type,
+            ngram_types=ngram_types,
             ngram_ids=ngram_ids,
             span_indices=span_indices,
         )
