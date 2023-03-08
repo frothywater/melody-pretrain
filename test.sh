ec3574dc582dbe3325a9e2baac7c4763896ebee2
frothywater <kokoroic@outlook.com>
2023-03-08 16:39:08 +0800

    train: New ablation

---
+++ test.sh
@@ -0,0 +1,21 @@
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_20 --ckpt_path experiment/model-rate/finetune_clm_span_20/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_30 --ckpt_path experiment/model-rate/finetune_clm_span_30/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_40 --ckpt_path experiment/model-rate/finetune_clm_span_40/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_50 --ckpt_path experiment/model-rate/finetune_clm_span_50/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_60 --ckpt_path experiment/model-rate/finetune_clm_span_60/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_70 --ckpt_path experiment/model-rate/finetune_clm_span_70/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model-rate/finetune_clm_span_80 --ckpt_path experiment/model-rate/finetune_clm_span_80/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model/finetune_clm_bar --ckpt_path experiment/model/finetune_clm_bar/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model/finetune_clm_ngram --ckpt_path experiment/model/finetune_clm_ngram/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model/finetune_clm_ngram_multi --ckpt_path experiment/model/finetune_clm_ngram_multi/lightning_logs/version_0/checkpoints/step=1000.ckpt
+
+python main.py test --config config/trainer.yaml --config config/model/base.yaml --config config/predict/test_clm.yaml --trainer.default_root_dir experiment/model/finetune_clm_single --ckpt_path experiment/model/finetune_clm_single/lightning_logs/version_0/checkpoints/step=1000.ckpt
