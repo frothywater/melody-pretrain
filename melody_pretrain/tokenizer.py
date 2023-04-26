53bc29d2934b6506afea455ffd24f96f196fb7ab
frothywater <kokoroic@outlook.com>
2023-04-26 16:37:39 +0800

    feat: Add <SEG> tokens for segment notes

--- melody_pretrain/tokenizer.py
+++ melody_pretrain/tokenizer.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 from math import floor, log2
 from typing import Dict, List, NamedTuple, Optional, Tuple, Union
 
@@ -33,6 +34,7 @@ class MIDITokenizer:
         self.pitch_range = range(pitch_range[0], pitch_range[1])
         self.ticks_per_bar = 1920
         self.units_per_bar = granularity
+        self.ticks_per_beat = self.ticks_per_bar // 4
         self.ticks_per_unit = self.ticks_per_bar // self.units_per_bar
         self.ticks_per_triplet_unit = self.ticks_per_unit // 3 * 4
 
@@ -55,10 +57,39 @@ class MIDITokenizer:
         self.vocab_sizes = [len(self.vocabularies[field_name]) for field_name in self.field_names]
         self.field_sizes = list(self.vocab_sizes)  # will be modified when adding special tokens
 
+        self.define_special_tokens()
         self.build_encoder_decoder()
+        self.set_special_token_ids()
 
     def define_vocabularies(self) -> None:
         raise NotImplementedError
+
+    def define_special_tokens(self) -> None:
+        self.bos_token_str = "<BOS>"
+        self.eos_token_str = "<EOS>"
+        self.pad_token_str = "<PAD>"
+        self.sep_token_str = "<SEP>"
+        self.cls_token_str = "<CLS>"
+        self.mask_token_str = "[MASK]"
+        self.long_mask_token_str = "[lMASK]"
+
+        self.bos_token = self.Token(*[self.bos_token_str] * len(self.field_names))
+        self.eos_token = self.Token(*[self.eos_token_str] * len(self.field_names))
+        self.pad_token = self.Token(*[self.pad_token_str] * len(self.field_names))
+        self.sep_token = self.Token(*[self.sep_token_str] * len(self.field_names))
+        self.cls_token = self.Token(*[self.cls_token_str] * len(self.field_names))
+        self.mask_token = self.Token(*[self.mask_token_str] * len(self.field_names))
+        self.long_mask_token = self.Token(*[self.long_mask_token_str] * len(self.field_names))
+
+        self.special_token_str = [
+            self.bos_token_str,
+            self.eos_token_str,
+            self.pad_token_str,
+            self.sep_token_str,
+            self.cls_token_str,
+            self.mask_token_str,
+            self.long_mask_token_str,
+        ]
 
     def build_encoder_decoder(self) -> None:
         self.encoder: Dict[str, Dict[AnyField, int]] = {
@@ -70,31 +101,6 @@ class MIDITokenizer:
             for field_name in self.field_names
         }
 
-        # special tokens
-        self.bos_token_str = "<BOS>"
-        self.eos_token_str = "<EOS>"
-        self.pad_token_str = "<PAD>"
-        self.sep_token_str = "<SEP>"
-        self.cls_token_str = "<CLS>"
-        self.mask_token_str = "[MASK]"
-        self.long_mask_token_str = "[lMASK]"
-        self.bos_token = self.Token(*[self.bos_token_str] * len(self.field_names))
-        self.eos_token = self.Token(*[self.eos_token_str] * len(self.field_names))
-        self.pad_token = self.Token(*[self.pad_token_str] * len(self.field_names))
-        self.sep_token = self.Token(*[self.sep_token_str] * len(self.field_names))
-        self.cls_token = self.Token(*[self.cls_token_str] * len(self.field_names))
-        self.mask_token = self.Token(*[self.mask_token_str] * len(self.field_names))
-        self.long_mask_token = self.Token(*[self.long_mask_token_str] * len(self.field_names))
-        self.special_token_str = [
-            self.bos_token_str,
-            self.eos_token_str,
-            self.pad_token_str,
-            self.sep_token_str,
-            self.cls_token_str,
-            self.mask_token_str,
-            self.long_mask_token_str,
-        ]
-
         # add special tokens to the encoder and decoder
         for field_index, field_name in enumerate(self.field_names):
             for i, token_str in enumerate(self.special_token_str):
@@ -103,7 +109,7 @@ class MIDITokenizer:
                 self.decoder[field_name][token_id] = token_str
             self.field_sizes[field_index] += len(self.special_token_str)
 
-        # convert special tokens to ids
+    def set_special_token_ids(self) -> None:
         self.bos_token_ids = self.convert_token_to_id(self.bos_token)
         self.eos_token_ids = self.convert_token_to_id(self.eos_token)
         self.pad_token_ids = self.convert_token_to_id(self.pad_token)
@@ -186,6 +192,80 @@ class MIDITokenizer:
         """Get the bar length of the tokens."""
         raise NotImplementedError
 
+    def get_segment_note_indices(self, midi: MidiFile) -> List[int]:
+        """Get indices of the segment notes of the midi file.
+        The segment notes are:
+        (1) the longest notes in each bar (longer than quarter note),
+        (2) notes following at least eighth rest."""
+
+        def filter_consecutive_notes(note_indices: List[int], notes: List[Note]):
+            if len(note_indices) <= 1:
+                return note_indices
+
+            note_indices = np.array(note_indices)
+            consecutive_mask = (note_indices[1:] - note_indices[:-1]) == 1
+            result = []
+            index = 0
+            while index < len(note_indices) - 1:
+                note_index = note_indices[index]
+                if not consecutive_mask[index]:
+                    result.append(note_index)
+                    if index == len(note_indices) - 2:
+                        # add the last note
+                        result.append(note_indices[-1])
+                    index += 1
+                else:
+                    # two consecutive notes
+                    cur_note = notes[note_index]
+                    next_note = notes[note_indices[index + 1]]
+                    distance = next_note.start - cur_note.end
+                    # if the distance between two consecutive notes is greater than eighth rest,
+                    # or the first note is longer than the second note, keep the first note;
+                    # otherwise, keep the second note.
+                    if distance >= self.tick_per_beat // 2:
+                        result.append(note_index)
+                    elif cur_note.end - cur_note.start > next_note.end - next_note.start:
+                        result.append(note_index)
+                    else:
+                        result.append(note_indices[index + 1])
+                    index += 2
+            return result
+
+        notes = midi.instruments[0].notes
+
+        # get the longest notes in each bar (longer than quarter note)
+        bar_dict = defaultdict(list)
+        for note in notes:
+            bar_dict[note.start // self.ticks_per_bar].append(note)
+        long_notes = []
+        for bar_notes in bar_dict.values():
+            max_duration = max(x.end - x.start for x in bar_notes)
+            if max_duration > self.ticks_per_beat:
+                long_notes.extend(note for note in bar_notes if note.end - note.start == max_duration)
+        long_note_indices = sorted(notes.index(note) for note in long_notes)
+        long_note_indices = filter_consecutive_notes(long_note_indices, notes)
+
+        # get notes following at least eighth rest
+        rest_note_indices = [
+            i
+            for i, note in enumerate(notes)
+            if i == len(notes) - 1 or notes[i + 1].start - note.end >= self.ticks_per_beat // 2
+        ]
+
+        # combine long notes and rest notes, then filter out consecutive notes
+        segment_note_indices = sorted(set(long_note_indices) | set(rest_note_indices))
+        segment_note_indices = filter_consecutive_notes(segment_note_indices, notes)
+
+        # if the first segment note is the first note, and is too close to the second note, remove the first note
+        if (
+            len(segment_note_indices) >= 2
+            and segment_note_indices[0] == 0
+            and notes[1].start - notes[0].end < self.ticks_per_beat // 2
+        ):
+            segment_note_indices = segment_note_indices[1:]
+
+        return segment_note_indices
+
     def _get_tempo_changes(self, midi: MidiFile) -> List[TempoChange]:
         # sort and deduplicate tempo changes
         tempo_changes = midi.tempo_changes
@@ -265,13 +345,28 @@ class OctupleTokenizer(MIDITokenizer):
         self.vocabularies["pitch"] = list(self.pitch_range)
         self.vocabularies["tempo"] = self.tempo_bins
 
+    def define_special_tokens(self) -> None:
+        super().define_special_tokens()
+        self.seg_token_str = "<SEG>"
+        self.seg_token = self.Token(*[self.seg_token_str] * len(self.field_names))
+        self.special_token_str.append(self.seg_token_str)
+
+    def set_special_token_ids(self) -> None:
+        super().set_special_token_ids()
+        self.seg_token_ids = self.convert_token_to_id(self.seg_token)
+
     def tokenize(self, midi: MidiFile) -> Tuple[List[Token], np.ndarray]:
         assert len(midi.instruments) == 1, "Only support single instrument midi file."
 
+        notes = midi.instruments[0].notes
+        segment_note_indices = self.get_segment_note_indices(midi)
+
         current_tempo_index = 0
         tempo_changes = self._get_tempo_changes(midi)
+        current_token_index = 0
         tokens: List[self.Token] = []
-        for note in midi.instruments[0].notes:
+        note_map = np.zeros(len(notes), dtype=note_map_record_dtype)
+        for note_index, note in enumerate(notes):
             # change current tempo if current note is after the next tempo change
             if (
                 current_tempo_index < len(tempo_changes) - 1
@@ -285,11 +380,18 @@ class OctupleTokenizer(MIDITokenizer):
             tempo = self._find_nearest(self.tempo_bins, tempo_changes[current_tempo_index].tempo)
             tokens.append(self.Token(bar, position, duration, note.pitch, tempo))
 
-        indices = np.arange(len(tokens))
-        note_map = np.zeros(len(tokens), dtype=note_map_record_dtype)
-        note_map["start"] = indices + 1  # +1 for the <BOS> token
-        note_map["end"] = indices + 2
+            # if note is a segment note, add a segment token
+            if note_index in segment_note_indices:
+                tokens.append(self.seg_token)
+                note_map[note_index] = (current_token_index, current_token_index + 2)
+                current_token_index += 2
+            else:
+                note_map[note_index] = (current_token_index, current_token_index + 1)
+                current_token_index += 1
+
         tokens = [self.bos_token] + tokens + [self.eos_token]
+        note_map["start"] += 1  # +1 for the <BOS> token
+        note_map["end"] += 1
         return tokens, note_map
 
     def detokenize(self, tokens: List[Token], velocity: int = 100) -> MidiFile:
@@ -611,13 +713,14 @@ class RemiTokenizer(MIDITokenizer):
 
 if __name__ == "__main__":
     # testing
-    tokenizer = CPTokenizer()
+    tokenizer = OctupleTokenizer()
     print("field_names:", tokenizer.field_names)
     print("field_indices:", tokenizer.field_indices)
     print("field_sizes:", tokenizer.field_sizes)
     print("vocab_sizes:", tokenizer.vocab_sizes)
     print("encoder:", tokenizer.encoder)
     midi = MidiFile("experiment/test/test.mid")
+
     token_ids, note_map = tokenizer.encode(midi)
 
     tokens = tokenizer.convert_ids_to_tokens(token_ids)
@@ -625,6 +728,14 @@ if __name__ == "__main__":
         print(token)
 
     print(note_map)
+    if (
+        np.all(note_map["end"][:-1] == note_map["start"][1:])
+        and note_map["start"][0] == 1
+        and note_map["end"][-1] == len(tokens) - 1
+    ):
+        print("Note map is continuous.")
+    else:
+        print("Note map is not continuous!")
 
     new_token_ids, _ = tokenizer.encode(tokenizer.decode(token_ids))
     if np.all(token_ids == new_token_ids):
