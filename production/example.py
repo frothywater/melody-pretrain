90202375afb4e438437900090be028ba3a82e2f5
frothywater <kokoroic@outlook.com>
2023-05-12 13:13:42 +0800

    feat: Inpainting for production

--- production/example.py
+++ production/example.py
@@ -1,12 +1,30 @@
 from model import MelodyGLM
 
-checkpoint_path = "experiment/production/melodyglm_finetuned_completion_small.ckpt"
-model = MelodyGLM.load_from_checkpoint(checkpoint_path, device="cuda:0")
-print("Model loaded successfully.")
 
-# Generate melody from scratch, with given sampling strategy
-result_midi = model.complete_melody(max_bar_length=32, temperature=1.2, top_k=5)
-result_midi.dump("experiment/production/result.mid")
+def melody_generation():
+    checkpoint_path = "melodyglm_base_completion.ckpt"
+    model = MelodyGLM.load_from_checkpoint(checkpoint_path, device="cuda:0")
 
-# Or generate melody with given prompt
-# result_midi = model.complete_melody("prompt.mid", max_bar_length=32, temperature=1.2, top_k=5)
+    # Generate melody from scratch, with given sampling strategy
+    result_midi = model.complete_melody(max_bar_length=32, temperature=0.9, top_k=10)
+
+    # Or generate melody with given prompt
+    result_midi = model.complete_melody("prompt.mid", max_bar_length=32, temperature=0.9, top_k=10)
+    result_midi.dump("result.mid")
+
+
+def melody_inpainting():
+    checkpoint_path = "melodyglm_base_inpainting.ckpt"
+    model = MelodyGLM.load_from_checkpoint(checkpoint_path, device="cuda:0")
+
+    # Inpaint melody with given prompt and bar range [start, end)
+    result_midi = model.inpaint_melody("prompt.mid", bar_range=(8, 16), temperature=0.9, top_k=10)
+
+    # Or with given note range [start, end)
+    result_midi = model.inpaint_melody("prompt.mid", note_range=(10, 20), temperature=0.9, top_k=10)
+
+    result_midi.dump("inpainting.mid")
+
+
+melody_generation()
+melody_inpainting()
